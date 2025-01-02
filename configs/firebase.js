fb096ce84f983b91dc271323db6d1b4b0dc44478
Udara Wijesinghe <udarawijesinghe20@gmail.com>
2025-01-03 00:05:20 +0530

    add the firebase keys to .env file

--- configs/firebase.js
+++ configs/firebase.js
@@ -1,10 +1,19 @@
+require('dotenv').config();
 const { initializeApp } = require('firebase/app');
 const { getAuth } = require('firebase/auth');
 const { getFirestore } = require('firebase/firestore');
 const { getStorage } =  require('firebase/storage');
 
 
-// const firebaseConfig = 
+const firebaseConfig = {
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
